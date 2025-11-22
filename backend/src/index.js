72af9893528cf2086ab4f31bfce2c95035321e2f
Amar Smaic <amarsmaic@gmail.com>
2025-11-22 22:03:04 +0100

    Added NBA API Endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,32 @@
-const express = require("express")
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import axios from "axios"
+
 const app = express();
-const axios = require("axios")
-require("dotenv").config()
+dotenv.config();
+
+import { BalldontlieAPI } from "@balldontlie/sdk"
+const nba_api = new BalldontlieAPI({});
 
 app.get("/", (req, res) => {
     res.send("Server radi")
 })
+
+app.get("/nba", async (req, res) => {
+  try {
+    const response = await axios.get("https://api.balldontlie.io/v1/teams", {
+      headers: {
+        Authorization: `Bearer ${process.env.NBA_API}`,
+      },
+    });
+
+    res.json(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 app.get("/football", async (req, res) => {
     try {
@@ -21,7 +42,6 @@ app.get("/football", async (req, res) => {
         res.status(500).send("Error");
     }
 });
-
 
 const PORT = 5000
 app.listen(PORT, () => {
