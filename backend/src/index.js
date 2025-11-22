859c60b1035f8535cdb2f7e38053b2c9cc75c206
Amar Smaic <amarsmaic@gmail.com>
2025-11-22 21:41:11 +0100

    Added football API endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,27 @@
 const express = require("express")
 const app = express();
+const axios = require("axios")
+require("dotenv").config()
 
 app.get("/", (req, res) => {
     res.send("Server radi")
 })
+
+app.get("/football", async (req, res) => {
+    try {
+        const response = await axios.get("https://api.football-data.org/v4/matches",
+            {
+                headers: {"X-Auth-Token": process.env.FOOTBALL_API}
+            }
+        );
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Error");
+    }
+});
+
 
 const PORT = 5000
 app.listen(PORT, () => {
